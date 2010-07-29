410fac54e5843bcf223291bdee6fe46d2c9ff172
Nathan Fixler <nathan@fixler.org>
2010-07-29 17:17:11 -0400

    Redirect to the index if the URL is bad or not present

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ get '/' do
 end
 
 get '/slides' do
-  @jpg_url, @position = params[:url].match(/(https?\:\/\/.+?)\/([\d]+)\.jpg/).captures
-  erb :slides
+  redirect '/' unless params[:url]
+  begin
+    @jpg_url, @position = params[:url].match(/(https?\:\/\/.+?)\/([\d]+)\.jpg/).captures
+    erb :slides
+  rescue NoMethodError
+    redirect '/'
+  end
 end
